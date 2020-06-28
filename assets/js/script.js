6532d0cf3de94f480dd05e056ad854129d0ed2d2
darshnablah <dwright758@gmail.com>
2020-06-28 00:27:33 -0500

    added 5 day info

--- assets/js/script.js
+++ assets/js/script.js
@@ -145,15 +145,120 @@ var displayUv = function (data) {
 
 var displayFiveDay = function (weather) {
     forecast.textContent = "";
+    fiveDay.textContent = "";
     var forecastHeader = document.createElement("h3");
     forecastHeader.textContent = "Five Day Forecast:";
     forecast.appendChild(forecastHeader);
-
-    var day = Math.floor((new Date()).getTime()/1000);
-    console.log(day);
-    console.log(typeof(day));
-    // for (var i = 0; i <)
     
+    // display first day
+    var day1 = document.createElement("div");
+    day1.className = "day";
+    var date1 = document.createElement("h5");
+    date1.textContent = moment().add(1, "day").format("MM/DD/YYYY");
+    day1.appendChild(date1);
+    console.log(weather.list[2].weather[0].icon);
+
+    var dayIcon1 = document.createElement("img");
+    dayIcon1.setAttribute("src", "http://openweathermap.org/img/w/" + weather.list[2].weather[0].icon + ".png");
+    day1.appendChild(dayIcon1);
+    
+
+    var temp1 = document.createElement("p");
+    temp1.textContent = "Temp: " + weather.list[2].main.temp;
+    day1.appendChild(temp1);
+
+    var humidity1 = document.createElement("p");
+    humidity1.textContent = "Humidity: " + weather.list[2].main.humidity + "%";
+    day1.appendChild(humidity1);
+    fiveDay.appendChild(day1);
+
+    // display second day
+    var day2 = document.createElement("div");
+    day2.className = "day";
+    var date2 = document.createElement("h5");
+    date2.textContent = moment().add(2, "day").format("MM/DD/YYYY");
+    day2.appendChild(date2);
+    
+
+    var dayIcon2 = document.createElement("img");
+    dayIcon2.setAttribute("src", "http://openweathermap.org/img/w/" + weather.list[10].weather[0].icon + ".png");
+    day2.appendChild(dayIcon2);
+    
+
+    var temp2 = document.createElement("p");
+    temp2.textContent = "Temp: " + weather.list[10].main.temp;
+    day2.appendChild(temp2);
+
+    var humidity2 = document.createElement("p");
+    humidity2.textContent = "Humidity: " + weather.list[10].main.humidity + "%";
+    day2.appendChild(humidity2);
+    fiveDay.appendChild(day2);
+
+    // display third day
+    var day3 = document.createElement("div");
+    day3.className = "day";
+    var date3 = document.createElement("h5");
+    date3.textContent = moment().add(3, "day").format("MM/DD/YYYY");
+    day3.appendChild(date3);
+    
+
+    var dayIcon3 = document.createElement("img");
+    dayIcon3.setAttribute("src", "http://openweathermap.org/img/w/" + weather.list[18].weather[0].icon + ".png");
+    day3.appendChild(dayIcon3);
+    
+
+    var temp3 = document.createElement("p");
+    temp3.textContent = "Temp: " + weather.list[18].main.temp;
+    day3.appendChild(temp3);
+
+    var humidity3 = document.createElement("p");
+    humidity3.textContent = "Humidity: " + weather.list[18].main.humidity + "%";
+    day3.appendChild(humidity3);
+    fiveDay.appendChild(day3);
+
+    // display fourth day
+    var day4 = document.createElement("div");
+    day4.className = "day";
+    var date4 = document.createElement("h5");
+    date4.textContent = moment().add(4, "day").format("MM/DD/YYYY");
+    day4.appendChild(date4);
+    
+
+    var dayIcon4 = document.createElement("img");
+    dayIcon4.setAttribute("src", "http://openweathermap.org/img/w/" + weather.list[26].weather[0].icon + ".png");
+    day4.appendChild(dayIcon4);
+    
+
+    var temp4 = document.createElement("p");
+    temp4.textContent = "Temp: " + weather.list[26].main.temp;
+    day4.appendChild(temp4);
+
+    var humidity4 = document.createElement("p");
+    humidity4.textContent = "Humidity: " + weather.list[26].main.humidity + "%";
+    day4.appendChild(humidity4);
+    fiveDay.appendChild(day4);
+
+    // display fifth day
+    var day5 = document.createElement("div");
+    day5.className = "day";
+    var date5 = document.createElement("h5");
+    date5.textContent = moment().add(5, "day").format("MM/DD/YYYY");
+    day5.appendChild(date5);
+    
+
+    var dayIcon5 = document.createElement("img");
+    dayIcon5.setAttribute("src", "http://openweathermap.org/img/w/" + weather.list[34].weather[0].icon + ".png");
+    day5.appendChild(dayIcon5);
+    
+
+    var temp5 = document.createElement("p");
+    temp5.textContent = "Temp: " + weather.list[34].main.temp;
+    day5.appendChild(temp5);
+
+    var humidity5 = document.createElement("p");
+    humidity5.textContent = "Humidity: " + weather.list[34].main.humidity + "%";
+    day5.appendChild(humidity5);
+    fiveDay.appendChild(day5);
 }
 
 button.addEventListener("click", buttonHandler)
@@ -164,4 +269,3 @@ searchCity.addEventListener("keyup", function (event) {
     }
 });
 searchHistory.addEventListener("click", historySearch);
-console.log(moment().add(1, "day").format("YYYY-MM-DD HH:mm:ss"));
